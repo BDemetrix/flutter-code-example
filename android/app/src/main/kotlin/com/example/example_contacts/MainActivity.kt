340c4c61f3032aaf29226d6acea03119a911e338
BDemetrix <b.demetrix@gmail.com>
2026-03-01 00:59:36 +0300

    Инициализирован Flutter

---
+++ android/app/src/main/kotlin/com/example/example_contacts/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.example_contacts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'core/di/launching.dart';
+import 'core/routes/router.dart';
+
+void main() async {
+  await bootstrap();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Example Contacts',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routerConfig: AppRouter.router,
+    );
+  }
+}
